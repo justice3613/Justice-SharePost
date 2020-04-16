1a327281b8f309cf2ac6eae2cef0eaeff124adbc
justice3613 <chimapeterjustize@gmail.com>
2020-04-16 16:17:20 +0100

    This is the first edit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '12345');
+  define('DB_NAME', 'sharepost');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/JUSTICESHAREPOST');
+  // Site Name
+  define('SITENAME', 'JUSTICE sharepost');
+  // App Version
+  define('APPVERSION', '1.0.0');
---
+++ app/views/inc/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark  btn-dark mb-3">
+  <div class="container">
+      <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo URLROOT; ?>"><strong>Home</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about"><strong>About</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>Service</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>Contacts</strong></a>
+          </li>
+        </ul>
+        
+        <ul class="navbar-nav ml-auto">
+          <?php if(isset($_SESSION['user_id'])) : ?>
+          <li class="nav-item">
+              <a class="nav-link" href="#"><strong>Welcome <?php echo $_SESSION['user_name']; ?></strong></a>
+            </li>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+            </li>
+          <?php else : ?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
